36beff9490f6fa4d4ccdf0aedc6b646ad18876cc
Steven Xu <steven@stevenxu.ca>
2011-06-08 20:08:48 -0400

    moved recipe location

--- lib/json_api_tester/core/runner.rb
+++ lib/json_api_tester/core/runner.rb
@@ -3,7 +3,6 @@ module JSONAPITester
 		class Runner
 			@@tester_path = File.join(JSONAPITester::Base.root, 'lib', 'json_api_tester', 'tests.rb')
 
-			# Run the specs found in 'lib/json_api_tester/spec/**/*_spec.rb'
 			def self.run
 				RSpec::Core::Runner.run(['--color', @@tester_path])
 			end
